3ff5b1d73fc8520cb5c47d7d47773b140ca70b50
Alex <skyriver.f1@gmail.com>
2022-11-18 22:09:41 +0500

    added articles detailed page & more

---
+++ marmelad/_blocks/article-detailed/article-detailed.js
@@ -0,0 +1,18 @@
+const galleryContentThumb = new Swiper(".js-gallery-content-thumb", {
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  watchSlidesProgress: true,
+  loop: true,
+});
+const galleryContentSlider = new Swiper(".js-gallery-content-slider", {
+  slidesPerView: 1,
+  spaceBetween: 72,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: galleryContentThumb,
+  },
+});
